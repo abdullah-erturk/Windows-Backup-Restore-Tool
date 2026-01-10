0c0af62dfbe8abdb9e1f9b496ab429a1f47e237c
Abdullah ERTÜRK <54866587+abdullah-erturk@users.noreply.github.com>
2026-01-10 19:06:57 +0300

    Source Code

---
+++ Windows Backup Restore Source/RecoveryTool/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Windows Backup / Restore Tool")]
+[assembly: AssemblyDescription("Windows Backup / Restore Tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Abdullah ERTÜRK")]
+[assembly: AssemblyProduct("BackupRestoreTool")]
+[assembly: AssemblyCopyright("Copyright © Abdullah ERTÜRK 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b89aa89d-c3e9-4c45-8c17-2d94b8f6c2e6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
